2ad21af37dc12685937a4b20cc19ce2ec6ef7a81
Petr Kubiznak <kubiznak.petr@elnico.cz>
2015-07-22 16:47:29 +0200

    Linux: network: More robust IP address retrieval; netmask added.

--- linux/network.cpp
+++ linux/network.cpp
@@ -14,8 +14,14 @@ int network_getIpStr (QString & sOut, const char * sIF) {
 
   sOut = "0.0.0.0";
   if (oIF.isValid()) {
-    sOut = oIF.addressEntries()[0].ip().toString();
-    return 0;
+    const QList<QNetworkAddressEntry> & oAddrList = oIF.addressEntries();
+    if (!oAddrList.empty()) {
+      sOut = oAddrList[0].ip().toString();
+      sOut.append("/");
+      sOut.append(QString("%1").arg(oAddrList[0].prefixLength()));
+      return 0;
+    }
+    return -2;
   } else {
     return -1;
   }
